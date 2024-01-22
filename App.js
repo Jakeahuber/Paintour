9369fe771c9fbb5d1de4ceb6a640a1bc4ec84213
Jake Huber <jah383@case.edu>
2024-01-22 14:37:53 -0500

    Added dark theme to navigation bar and home tab. Added top header that says Daily Sketch. Changed demo sketch image on home page

--- App.js
+++ App.js
@@ -1,41 +1,55 @@
 import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import Home from './components/home'
 import Profile from './components/profile'
 import Search from './components/search'
 
+
 const homeName = 'Home';
 const profileName = 'Profile';
 const searchName = 'Search';
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-        <Tab.Navigator initialRouteName={homeName} screenOptions={({route}) => ({
-            tabBarIcon: ({focused, color, size}) => {
-                let iconName;
-                if (route.name === homeName) {
-                    iconName = focused ? 'home' : 'home-outline'
-                }
-                else if (route.name === profileName) {
-                    iconName = focused ? 'list' : 'list-outline'
-                }
-                else if (route.name === searchName) {
-                    iconName = focused ? 'settings' : 'settings-outline'
-                }
-                return <Ionicons name={iconName} size={size} color={color}/>
-            },
-            tabBarActiveTintColor: '#000000',
-            tabBarInactiveTintColor: 'gray',
-        })}
-        >
-            <Tab.Screen name={homeName} component={Home}/>
-            <Tab.Screen name={profileName} component={Profile}/>
-            <Tab.Screen name={searchName} component={Search}/>
-        </Tab.Navigator>
-    </NavigationContainer>
+    <>
+        <NavigationContainer theme={{colors: {background: 'black'}}}>
+            <Tab.Navigator initialRouteName={homeName} screenOptions={({route}) => ({
+                tabBarIcon: ({focused, color, size}) => {
+                    let iconName;
+                    if (route.name === homeName) {
+                        iconName = focused ? 'home' : 'home-outline'
+                    }
+                    else if (route.name === profileName) {
+                        iconName = focused ? 'person' : 'person-outline'
+                    }
+                    else if (route.name === searchName) {
+                        iconName = focused ? 'search' : 'search-outline'
+                    }
+                    return <Ionicons name={iconName} size={size} color={color}/>
+                },
+                tabBarActiveTintColor: 'white',
+                tabBarInactiveTintColor: 'gray',
+                tabBarStyle: {
+                    backgroundColor: 'black',
+                },
+                headerTitle: 'Daily Sketch',
+                headerTitleStyle: {
+                    color: 'white',
+                    fontSize: 28
+                },
+                headerTitleAlign: 'left',
+            })}
+            >
+                <Tab.Screen name={homeName} component={Home}/>
+                <Tab.Screen name={profileName} component={Profile} options={{ headerShown: false}}/>
+                <Tab.Screen name={searchName} component={Search} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    </>
   );
 }
